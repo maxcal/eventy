8bd95217543c4e2290a0561f8dffb2f05d34953b
Max Calabrese <max.calabrese@ymail.com>
2023-10-02 15:50:50 +0200

    add test for event presence validation

--- test/models/venue_test.rb
+++ test/models/venue_test.rb
@@ -4,4 +4,10 @@ class VenueTest < ActiveSupport::TestCase
   test "belongs_to event" do
     assert_equal :belongs_to, Venue.reflect_on_association(:event)&.macro
   end 
+
+  test "event is required" do
+    venue = Venue.new 
+    venue.valid? # fire the validations
+    assert_includes venue.errors[:event], "must exist"
+  end
 end
